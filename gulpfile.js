0831723ec72b64ba8102068fc2463638fc713df6
Henrique Ramos Limas <henrique.ramos.limas@gmail.com>
2015-06-10 19:29:02 -0300

    chore(gulpfile) add build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var karma = require('karma').server;
+var runSequence = require('run-sequence');
 
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
@@ -8,11 +9,12 @@ var path = {
     src: './src/*.js',
     tests: './tests/*.js'
 };
-gulp.task('test:unit', function() {
+
+gulp.task('test:unit', function(done) {
     karma.start({
         configFile: __dirname + '/karma.conf.js',
         singleRun: true
-    });
+    }, done);
 });
 
 gulp.task('test', ['test:unit']);
@@ -21,4 +23,10 @@ gulp.task('jshint', function() {
     gulp.src([path.src, path.tests])
         .pipe(jshint())
         .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('build', ['jshint'], function(done) {
+    return runSequence(
+        'test',
+        done);
 });
