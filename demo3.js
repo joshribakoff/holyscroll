0e180777732459cf116e35659a1bf569a48a384b
Josh Ribakoff <josh.ribakoff@gmail.com>
2015-06-10 08:23:43 -0700

    Update demo3 with "best practice" example of how to call into the calling scope from within the page template, using scope events within AngularJS.

--- demo3.js
+++ demo3.js
@@ -7,6 +7,13 @@ angular.module('phonecatApp', ['ui.bootstrap', 'holyscroll'])
     $scope.maxSize = 5;
     $scope.totalPages = 175;
 
+    $scope.$on('showItem', function(e, eventData) {
+        console.log(eventData);
+        var msg = "Controller received event from page scope. Do you wish to navigate to " + eventData.data.url;
+        if(confirm(msg)) {
+            window.location = eventData.data.url;
+        }
+    });
 
     $scope.loadPhones = function(page, cb) {
         var url = "http://api.reddit.com/hot?after=" + nextPage + "&jsonp=JSON_CALLBACK";
